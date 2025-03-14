4835132672a69c108afdb7d316b535b57f2c41fd
AAperghis <albert@aperghis.nl>
2025-03-14 14:21:58 +0100

    NURBS plots

--- import_nurbs.jl
+++ import_nurbs.jl
@@ -1,4 +1,7 @@
 using NURBS, FileIO, PlotlyJS
 
 patches = load(joinpath(@__DIR__, "data", "Model_ps.stp"))
-plotPatches(patches, plotControlPoints=false, resolution=0.1)
+p=plotPatches(patches, plotControlPoints=false, resolution=0.1)
+savefig(p, "nurbs_import.png")  # PNG format
+savefig(p, "nurbs_import.html") # Interactive HTML (best for 3D)
+display(p)
